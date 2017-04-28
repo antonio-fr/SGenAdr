e1ac03a451b634875f6ffa191f020f4408a1d89c
a_ferron <anferron@gmail.com>
2017-04-28 14:10:32 +0200

    Update
    NanoS not mandatory, Ask for confirmation
    Add random in PC, get more random from NanoS

--- SGenAdr.py
+++ SGenAdr.py
@@ -26,6 +26,7 @@ from lib.ECDSA_BTC import *
 import hashlib
 from ledgerblue.comm import getDongle
 from ledgerblue.commException import CommException
+import random
 
 
 def compute_adr(priv_num):
@@ -43,21 +44,39 @@ if __name__ == '__main__':
 		numadr = int(arg1)
 	except:
 		raise ValueError("Error in arguments\nUse :     SGenAdr.py <number of adresses to generate>\n]")
-	print "\n--- Generate %i new Bitcoin accounts ---\n" % numadr
 	load_gtable('lib/G_Table')
-	nanos_dongle = getDongle()
-	rawversions = nanos_dongle.exchange(bytes("E0C4000000".decode('hex')))
-	version = rawversions[2:5]
-	print "Nano S dongle detected\n version %s\n" % ".".join(map(str, version))
-	# Improvement : perform signature attestation genuine check
-	getrandom = "E0C000"+"0200" # 0x0200 bytes = 512 bytes = 16 x 256b
+	use_nano = True
+	try:
+		nanos_dongle = getDongle()
+		rawversions = nanos_dongle.exchange(bytes("E0C4000000".decode('hex')))
+		version = rawversions[2:5]
+		print "\nNano S dongle detected\n version %s\n" % ".".join(map(str, version))
+	except:
+		print "\n--- WARNING : No NanoS connected in Bitcoin Mode ---"
+		cont = raw_input("Would you like to continue using RNG from PC OS? (Y/[N])")
+		if cont!="Y":
+			quit()
+		use_nano = False
+	getrandom = "E0C000"+"00C0" # 0x00C0 bytes = 192 bytes = 6 x 256b
+	print "\n--- Generating %i new Bitcoin accounts ---\n" % numadr
 	f = open('address_list.csv', 'wb')
 	f.write("index,pubkey,address,prvkeyWIP\r\n")
+	rndg = random.SystemRandom()
 	for i in xrange(numadr):
 		candint = 0
 		while candint<1 or candint>=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141L:
-			random_from_nanos = nanos_dongle.exchange(bytes(getrandom.decode('hex')))
-			cand = hashlib.sha256(random_from_nanos).hexdigest()
+			if use_nano:
+				random1_from_nanos = bytes(nanos_dongle.exchange(bytes(getrandom.decode('hex')))) # Get 12 chucks of 256 random bits from PTG2 TRNG
+				random2_from_nanos = bytes(nanos_dongle.exchange(bytes(getrandom.decode('hex')))) #     6x2
+				random_from_nanos = random1_from_nanos + random2_from_nanos
+			else:
+				random_from_nanos = bytes(os.urandom(384))
+			assert random_from_nanos.__len__() == 384
+			rnd_nano_list = []
+			for x in range(12):
+				rnd_nano_list.append(random_from_nanos[32*x:32*(x+1)])
+			sel_random = rndg.sample(rnd_nano_list, 8)             # Get 8 random chuncks out of the 12
+			cand = hashlib.sha256("".join(sel_random)).hexdigest() # get 256 bits from 256 random-source bytes
 			candint = int(cand,16)
 		privkeynum = candint
 		pvkeywip = priv_hex_base58(privkeynum)
