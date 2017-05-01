cbc0f3228ca83c82f33831e1ec4de65efa0cee10
a_ferron <anferron@gmail.com>
2017-05-01 17:57:38 +0200

    Fix and improve Debian script

--- debian-install.sh
+++ debian-install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -x
-# Install on Debian Live
+# Install SGenAdr on Debian Live
+# run with : sudo bash -x debian-install.sh
 
-sudo apt-get install python-dev libusb-1.0-0-dev libudev-dev python-pip git
-sudo pip install --upgrade setuptools
+apt-get -y install python-dev libusb-1.0-0-dev libudev-dev python-pip git
+pip install --upgrade setuptools
 git clone https://github.com/trezor/cython-hidapi.git
 cd cython-hidapi
 git submodule update --init
 python setup.py build
-sudo python setup.py install
+python setup.py install
 cd..
 pip install ledgerblue
